4caef872c8f0a909722a9820c4a71d78c8184268
Pallat Anchaleechamaikorn <yod.pallat@gmail.com>
2026-03-15 17:23:27 +0700

    add dockerfile and makefile fir clang

--- clang101/main.c
+++ clang101/main.c
@@ -1,10 +1,11 @@
 #include <microhttpd.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #define PORT 8888
 
-int answer_to_connection(void *cls, struct MHD_Connection *connection, 
+enum MHD_Result answer_to_connection(void *cls, struct MHD_Connection *connection, 
                          const char *url, const char *method, const char *version, 
                          const char *upload_data, size_t *upload_data_size, void **con_cls) {
     const char *page = "<html><body>Hello, World!</body></html>";
@@ -15,7 +16,10 @@ int answer_to_connection(void *cls, struct MHD_Connection *connection,
     ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
     MHD_destroy_response(response);
 
-    return ret;
+    if (ret != MHD_YES) {
+        return MHD_NO;
+    }
+    return MHD_YES;
 }
 
 int main() {
@@ -26,7 +30,9 @@ int main() {
     if (NULL == daemon) return 1;
 
     printf("Server running on port %d\n", PORT);
-    getchar(); // Wait for user input to stop the server
+    while (1) {
+        sleep(1);
+    }
 
     MHD_stop_daemon(daemon);
 
